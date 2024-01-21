188ee561bdb8f46edee421132f261fd774f81019
Joshua <joshuahsu108@gmail.com>
2024-01-20 18:46:20 -0800

    Add player

--- src/map/MainPage.tsx
+++ src/map/MainPage.tsx
@@ -3,6 +3,7 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
 import { MapControls } from "@react-three/drei";
 import * as THREE from "three";
+import { Model } from '../assets/Model'
 
 const temp = new THREE.Object3D();
 const material = new THREE.MeshPhongMaterial({ color: "red" });
@@ -27,11 +28,32 @@ const Balls = ({ locations }: { locations: [number, number][] }) => {
     <instancedMesh ref={ref} args={[geometry, material, locations.length]} />
   );
 };
+const Player = ({ locations }: { locations: [number, number][] }) => {
+  const ref = useRef<THREE.InstancedMesh>(null);
+
+  useFrame(() => {
+    if (!ref || !ref.current) return;
+    let counter = 0;
+    for (const [x, y] of locations) {
+      const id = counter++;
+      temp.position.set(x, y, 0);
+      temp.rotation.set(0, 0, 0);
+      temp.updateMatrix();
+      ref.current.setMatrixAt(id, temp.matrix);
+    }
+    ref.current.instanceMatrix.needsUpdate = true;
+  });
+
+  return (
+    <instancedMesh ref={ref} args={[geometry, material, locations.length]} />
+  );
+};
 
 function MainPage() {
   return (
     <div className="w-full h-screen">
       <Canvas className="bg-black h-full">
+        <Model />
         <Perf />
         {/* <PerspectiveCamera makeDefault /> */}
         {/* <PresentationControls> */}
@@ -40,11 +62,6 @@ function MainPage() {
         <ambientLight intensity={0.5} />
         <directionalLight color="red" position={[0, 0, 5]} />
         <directionalLight color="red" position={[0, 5, 5]} />
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
-        <Balls locations={[[0, 1]]} />
 
         {/* </PresentationControls> */}
       </Canvas>
